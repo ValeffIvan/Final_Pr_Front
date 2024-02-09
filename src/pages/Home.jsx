3832bb756b7c3381cc542600111766f3c6bcb188
Ivan Valeff <ivaleff@gmail.com>
2024-02-09 00:37:05 -0300

    Inicio

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import About from '../components/Home/About';
+
+
+const Home = () => {
+  return (
+    <section>      
+      <About />
+    </section>
+  );
+}
+
+export default Home
