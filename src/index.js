75613e036b6ba846d1f3529f878483f062ef37c0
Ivan Valeff <ivaleff@gmail.com>
2024-02-29 05:12:22 -0300

    clear

--- src/index.js
+++ src/index.js
@@ -2,12 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { App } from './App';
-import { config } from 'react-dotenv';
-
-config({
-  path: './.env',
-  name: 'REACT_APP',
-});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
